7eacd9570ed0f1c4ab33a31c4494ad869be998a1
Santosh <97425696+Sekharsantosh@users.noreply.github.com>
2022-05-20 07:45:38 +0530

    n=int(input()) for i  in range(1,n+1):     for j in range(1,n-1):#5//2=2+1           print(j,end="")     for k in reversed(range(1,j)):#santosh coding is fun           print(k,end="")     print()

--- print_the_following_pattern.py
+++ print_the_following_pattern.py
@@ -2,6 +2,6 @@ n=int(input())
 for i  in range(1,n+1):
     for j in range(1,n-1):#5//2=2+1
           print(j,end="")
-    for k in range(1,j):
+    for k in reversed(range(1,j)):#santosh coding is fun
           print(k,end="")
     print()

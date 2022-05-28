674fe1b3217b961ecf21020d7f1524669a3e32c8
Santosh <97425696+Sekharsantosh@users.noreply.github.com>
2022-05-28 11:56:37 +0530

    n=int(input()) a=list(map(int,input().split())) s,p=map(int,input().split()) b=[] c=0 for i in a:     if i in range(s,p+1):         b.append(i)         c=c+1 if c==0:     print("-1") else:     print(*b)

---
+++ elements_in_between_A_and_B.py
@@ -0,0 +1,15 @@
+n=int(input())
+a=list(map(int,input().split()))
+s,p=map(int,input().split())
+b=[]
+c=0
+for i in a:
+    if i in range(s,p+1):
+        b.append(i)
+        c=c+1
+if c==0:
+    print("-1")
+else:
+    print(*b)
+    
+    

c6f35bff58df79bb26a240fa0f35a93309175c29
Santosh <97425696+Sekharsantosh@users.noreply.github.com>
2022-06-02 14:28:24 +0530

    n=int(input()) v="ABCDEFGHIJKLMNOPQRSSTUVWXYZ" for i in range(n):     for j in range(n):             print(v[i],end=" ")     print()

--- print_the_following_pattern.py
+++ print_the_following_pattern.py
@@ -1,7 +1,6 @@
 n=int(input())
-for i  in range(1,n+1):
-    for j in range(1,n-1):#5//2=2+1
-          print(j,end="")
-    for k in reversed(range(1,j)):#santosh coding is fun
-          print(k,end="")
+v="ABCDEFGHIJKLMNOPQRSSTUVWXYZ"
+for i in range(n):
+    for j in range(n):
+            print(v[i],end=" ")
     print()

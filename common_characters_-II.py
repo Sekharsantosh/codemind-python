53881f40957008bd02fa03fc4cab741b17c4040f
Santosh <97425696+Sekharsantosh@users.noreply.github.com>
2022-06-10 19:47:27 +0530

    n=input() p=input() n=n.split() n="".join(n) n=n.lower() p=p.split() p="".join(p) p=p.lower() c=0
    n=set(n)
    p=set(p)
    for i in n:
        if i in p:
            c=c+1
    print(c)

---
+++ common_characters_-II.py
@@ -0,0 +1,16 @@
+n=input()
+p=input()
+n=n.split()
+n="".join(n)
+n=n.lower()
+p=p.split()
+p="".join(p)
+p=p.lower()
+c=0
+
+n=set(n)
+p=set(p)
+for i in n:
+    if i in p:
+        c=c+1
+print(c)

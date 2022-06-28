c5eb7b0326233bcdaf0aab07a373169c4066a786
Santosh <97425696+Sekharsantosh@users.noreply.github.com>
2022-06-28 21:16:05 +0530

    n=int(input()) a=0 b=1 e=0 d=[] for i in range(2,n):     c=a+b     a=b     b=c     d.append(c) for i in d:     if n in d:         e=1     else:         e=0 if e==1:     print("True") else:     print("False")

--- Fibonacci_or_not.py
+++ Fibonacci_or_not.py
@@ -1,16 +1,19 @@
-def fib(n):
-    a=0
-    b=1
-    for i in range(3,n):
-            c=a+b
-            if c==n:
-                break
-            a=b
-            b=c
-    
-    if c==n:
-        return True
-    else:
-        return False
 n=int(input())
-print(fib(n))
+a=0
+b=1
+e=0
+d=[]
+for i in range(2,n):
+    c=a+b
+    a=b
+    b=c
+    d.append(c)
+for i in d:
+    if n in d:
+        e=1
+    else:
+        e=0
+if e==1:
+    print("True")
+else:
+    print("False")

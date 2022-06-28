fba6c2f22866d005361481e4522d9af3bff62044
Santosh <97425696+Sekharsantosh@users.noreply.github.com>
2022-06-28 19:34:10 +0530

    def amica(t):     s=0     for i in range(1,n):         if n%i==0:             s=s+i     return s n=int(input()) m=int(input()) #print(n,m) p=amica(n) k=amica(m) #print(k,p) if p==m or k==n:     print("Amicable") else:     print("Not Amicable")

--- Amicable_Numbers.py
+++ Amicable_Numbers.py
@@ -1,14 +1,16 @@
+def amica(t):
+    s=0
+    for i in range(1,n):
+        if n%i==0:
+            s=s+i
+    return s
 n=int(input())
-s=0
-for i in range(1,n):
-    if n%i==0:
-        s=s+i
-p=int(input())
-k=0
-for a in range(1,p):
-    if p%a==0:
-        k=k+a
-if s==p and k==n:
+m=int(input())
+#print(n,m)
+p=amica(n)
+k=amica(m)
+#print(k,p)
+if p==m or k==n:
     print("Amicable")
 else:
     print("Not Amicable")

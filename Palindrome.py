172c652cebf325b5ca072e6055df3b84e4108fb2
Santosh <97425696+Sekharsantosh@users.noreply.github.com>
2022-06-28 22:07:50 +0530

    def reverse(n):     t=n     rev=0     while(t):         r=t%10         rev=rev*10+r         t=t//10     if rev==n:         return True     else:         return False n=int(input()) if(reverse(n)):     print(True) else:     print(False)

--- Palindrome.py
+++ Palindrome.py
@@ -1,11 +1,16 @@
+def reverse(n):
+    t=n
+    rev=0
+    while(t):
+        r=t%10
+        rev=rev*10+r
+        t=t//10
+    if rev==n:
+        return True
+    else:
+        return False
 n=int(input())
-e=n
-rev=0
-while e:
-    r=e%10
-    rev=rev*10+r
-    e=e//10
-if(rev==n):
-    print("True")
+if(reverse(n)):
+    print(True)
 else:
-    print("False")
+    print(False)

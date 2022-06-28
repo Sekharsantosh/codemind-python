89613ec771243d54175f9a2d58d8b57901cbad11
Santosh <97425696+Sekharsantosh@users.noreply.github.com>
2022-06-28 22:05:13 +0530

    def fact(n):      s=0      for i in range(1,n):          if n%i==0:              s=s+i      if s>n:          return True      else:          return False n=int(input()) if fact(n):     print("True") else:     print(False)

--- Abundant_Numbers.py
+++ Abundant_Numbers.py
@@ -1,9 +1,14 @@
+def fact(n):
+     s=0
+     for i in range(1,n):
+         if n%i==0:
+             s=s+i
+     if s>n:
+         return True
+     else:
+         return False
 n=int(input())
-c=0
-for i in range(1,n):
-    if(n%i==0):
-        c=c+i
-if(c>n):
+if fact(n):
     print("True")
 else:
-    print("False")
+    print(False)

b0ba00543dbdd6d5ed75dd245a9e0f5249b915e3
Santosh <97425696+Sekharsantosh@users.noreply.github.com>
2022-06-28 15:56:28 +0530

    n=int(input()) c=0 k=0 for i in range(1,n//2):     if n%i==0:         c=i     if c*(c-1)==n:         k=1 if k==1:     print("YES") else:     print("NO")

--- Pronic_Number_or_Heteromecic_Number.py
+++ Pronic_Number_or_Heteromecic_Number.py
@@ -1,9 +1,12 @@
 n=int(input())
-import  math
-i=math.sqrt(n)
-k=(round(i))
-p=k+1
-if k*p==n:
+c=0
+k=0
+for i in range(1,n//2):
+    if n%i==0:
+        c=i
+    if c*(c-1)==n:
+        k=1
+if k==1:
     print("YES")
 else:
     print("NO")

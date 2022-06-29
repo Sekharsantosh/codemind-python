bffdc591543b065f8b5bafbd4bb11bc2028c5e36
Santosh <97425696+Sekharsantosh@users.noreply.github.com>
2022-06-29 21:16:12 +0530

    def reverse(n):     t=n     rev=0     while(t):         r=t%10         rev=rev+r         t=t//10     return rev n=int(input()) a=list(map(int,input().split())) c=0 for i in a:       c=c+reverse(i) print(c)

--- Play_with_digits.py
+++ Play_with_digits.py
@@ -1,12 +1,14 @@
+def reverse(n):
+    t=n
+    rev=0
+    while(t):
+        r=t%10
+        rev=rev+r
+        t=t//10
+    return rev
 n=int(input())
 a=list(map(int,input().split()))
-s=0
+c=0
 for i in a:
-    if i<10:
-        s=s+i
-    else :
-        while i!=0:
-            r=i%10
-            s=s+r
-            i=i//10
-print(s)
+      c=c+reverse(i)
+print(c)

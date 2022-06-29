ca57b13ce03a9224b386fa9ce57827ae58825364
Santosh <97425696+Sekharsantosh@users.noreply.github.com>
2022-06-29 10:19:50 +0530

    def count(n):     c=0     if n==0 or n==1:         c=c+1         return c     while(n):         r=n%10         c=c+1         n=n//10     return c n=int(input()) a=list(map(int,input().split())) k=0 b=[] #print(*a) for i in a:     if i<0:         i=i*(-1)     k=count(i)     b.append(k) print(*b)

--- count_digits.py
+++ count_digits.py
@@ -1,9 +1,22 @@
+def count(n):
+    c=0
+    if n==0 or n==1:
+        c=c+1
+        return c
+    while(n):
+        r=n%10
+        c=c+1
+        n=n//10
+    return c
 n=int(input())
 a=list(map(int,input().split()))
+k=0
+b=[]
+#print(*a)
 for i in a:
-       if i<0:
-           i=i*(-1)
-       else:
-           i=i*1
-       k=len(str(i))
-       print(k,end=" ")
+    if i<0:
+        i=i*(-1)
+    k=count(i)
+    b.append(k)
+print(*b)
+    

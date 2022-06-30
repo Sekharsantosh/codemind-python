2f00b2942721735c8d6c9e63c4c329911c804e7b
Santosh <97425696+Sekharsantosh@users.noreply.github.com>
2022-06-30 11:57:19 +0530

    n=int(input()) x=list(map(int,input().split())) s=0 c=0 for i in x:       if i==0 or i==1:          c=1       else:            c=0            break if c==1:     print(True) else:     print(False)

--- Binary_Array.py
+++ Binary_Array.py
@@ -1,14 +1,13 @@
 n=int(input())
+x=list(map(int,input().split()))
+s=0
 c=0
-a=list(map(int,input().split()))
-k=set(a)
-for i in k:
-    #print(i)
-    if(i>=0 and i<=1):
-        c=1
-    elif(i>1):
-        c=0
-        break
+for i in x:
+      if i==0 or i==1:
+         c=1
+      else:
+           c=0
+           break
 if c==1:
     print(True)
 else:

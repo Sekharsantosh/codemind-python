d8a8985e809f56fdb2e634c5564537ff61dd0ec3
Santosh <97425696+Sekharsantosh@users.noreply.github.com>
2022-06-30 09:40:45 +0530

    n,m=map(int,input().split()) a=list(map(int,input().split())) b=list(map(int,input().split())) a=set(a) b=set(b) c=0#solved by santosh for i in a:      if i not in b:          c=c+1 for j in b:     if j not in a:         c=c+1 print(c)

--- Count_elements_II.py
+++ Count_elements_II.py
@@ -3,11 +3,11 @@ a=list(map(int,input().split()))
 b=list(map(int,input().split()))
 a=set(a)
 b=set(b)
-c=0
+c=0#solved by santosh
 for i in a:
-    if i not in b:
-        c=c+1
-for i in b:
-    if i not in a:
+     if i not in b:
+         c=c+1
+for j in b:
+    if j not in a:
         c=c+1
 print(c)

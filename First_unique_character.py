3896e7db16759d0c2e22647468cf2edd3b02bb7e
Santosh <97425696+Sekharsantosh@users.noreply.github.com>
2022-06-30 18:56:40 +0530

    n=input() n=n.lower() n=n.split() n="".join(n) c=0 for i in n:     if n.count(i)==1:         c=i         break if c==0:     print(-1) else:     print(c)

--- First_unique_character.py
+++ First_unique_character.py
@@ -1,12 +1,13 @@
 n=input()
+n=n.lower()
 n=n.split()
 n="".join(n)
-n=n.lower()
 c=0
 for i in n:
-    if n.count(i)==1 and i.isalpha():
-        print(i,end="")
-        c=1
+    if n.count(i)==1:
+        c=i
         break
 if c==0:
     print(-1)
+else:
+    print(c)

b5f2d4945acbe60f364706f53d436a4b4cb8ba69
Santosh <97425696+Sekharsantosh@users.noreply.github.com>
2022-08-21 10:58:08 +0530

    n=int(input()) s=str(n) k=len(s) f=n*n if (f%10**k==n):     print("Automorphic Number") else:     print("Not an Automorphic Number")

--- Automorphic_Number.py
+++ Automorphic_Number.py
@@ -1,16 +1,8 @@
 n=int(input())
-e=n
-k=e*e
-c=0
-t=10
-while(e):
-    r=k%t
-    if(n==r):
-        c=1
-        break
-    e=e//10
-    t=t*10
-if(c==1):
+s=str(n)
+k=len(s)
+f=n*n
+if (f%10**k==n):
     print("Automorphic Number")
 else:
     print("Not an Automorphic Number")

bab6bc36a146459568dad942f6de6035ecb09266
Santosh <97425696+Sekharsantosh@users.noreply.github.com>
2022-08-22 15:55:05 +0530

    def is_prime(n):     if n==1:         return False     for i in range(2,int(n**0.5)+1):          if n%i==0:              return False     return True def reverse(n):     rev=0     while(n):         r=n%10         rev=rev*10+r         n=n//10     return rev n=int(input()) k=0 j=0 if is_prime(n):     k=1     if k==1:         n=reverse(n)         if is_prime(n):              j=1 if k==1 and j==1:     print("circular prime") elif k==1 and j!=1:     print("prime but not a circular prime") else:     print("not prime")

--- circular_prime.py
+++ circular_prime.py
@@ -1,40 +1,31 @@
-def reverse(n):
-    rev=0
-    while(n):
-       r=n%10
-       rev=rev*10+r
-       n=n//10
-    return rev
 def is_prime(n):
     if n==1:
         return False
     for i in range(2,int(n**0.5)+1):
-            if n%i==0:
-                return False
+         if n%i==0:
+             return False
     return True
+def reverse(n):
+    rev=0
+    while(n):
+        r=n%10
+        rev=rev*10+r
+        n=n//10
+    return rev
 n=int(input())
 k=0
-f=reverse(n)
+j=0
 if is_prime(n):
-    if is_prime(f):
-           print("circular prime")
-    else:
-        print("prime but not a circular prime")
+    k=1
+    if k==1:
+        n=reverse(n)
+        if is_prime(n):
+             j=1
+if k==1 and j==1:
+    print("circular prime")
+elif k==1 and j!=1:
+    print("prime but not a circular prime")
 else:
     print("not prime")
-
-
-
-
-
-
     
-
-
-
-
-
-
-
-
-        
+    

253b9c4b216be3d9c7b907517db3533ba9166471
Santosh <97425696+Sekharsantosh@users.noreply.github.com>
2022-08-22 15:46:58 +0530

    def pal(n):     rev=0     t=n     while t:         r=t%10         rev=rev*10+r         t=t//10     if n==rev:         return True     else:         return False def is_prime(n):     if n==1:         return False     for i in range(2,int(n**0.5)+1):         if n%i==0:             return False     return True n=int(input()) while True:     n=n+1     if pal(n):         if is_prime(n):             print(n)             break

--- Program_to_find_out_the_next_prime_palindrome_number.py
+++ Program_to_find_out_the_next_prime_palindrome_number.py
@@ -1,35 +1,26 @@
-def is_palindrome(n):
+def pal(n):
     rev=0
     t=n
-    while(t):
+    while t:
         r=t%10
         rev=rev*10+r
         t=t//10
-    return rev
+    if n==rev:
+        return True
+    else:
+        return False
 def is_prime(n):
     if n==1:
         return False
     for i in range(2,int(n**0.5)+1):
-            if n%i==0:
-                return False
+        if n%i==0:
+            return False
     return True
 n=int(input())
-k=0
-if n>0:
-   n=n+1
-while(n):
-    t=is_palindrome(n)
-    k=is_prime(n)
-    if t==n and k==True :
+while True:
+    n=n+1
+    if pal(n):
+        if is_prime(n):
             print(n)
             break
-    elif t!=n or(k!=True):
-       n=n+1
-
-
-
-
-
-
-
         

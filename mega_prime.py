60f8b651dd7a493bc9733bb13826f451e2fba57d
Santosh <97425696+Sekharsantosh@users.noreply.github.com>
2022-08-24 13:08:05 +0530

    def is_prime(n):     if n==1:         return False     for i in range(2,int(n**0.5)+1):         if n%i==0:             return False     return True a=int(input()) k=0 t=0 if is_prime(a):     t=a     while t!=0:         k=0         r=t%10         if is_prime(r):             k=1             #print(r)             t=t//10         else:             break if k==1:         print("Mega Prime") else:     print("Not Mega Prime")

--- mega_prime.py
+++ mega_prime.py
@@ -2,37 +2,25 @@ def is_prime(n):
     if n==1:
         return False
     for i in range(2,int(n**0.5)+1):
-            if n%i==0:
-                return False
+        if n%i==0:
+            return False
     return True
-n=int(input())
+a=int(input())
 k=0
-if is_prime(n):
-    t=n
-    while(t):
+t=0
+if is_prime(a):
+    t=a
+    while t!=0:
+        k=0
         r=t%10
         if is_prime(r):
             k=1
+            #print(r)
+            t=t//10
         else:
-            k=0
             break
-        t=t//10
 if k==1:
-    print("Mega Prime")
+        print("Mega Prime")
 else:
     print("Not Mega Prime")
 
-
-
-
-
-    
-
-
-
-
-
-
-
-
-        

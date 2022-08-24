31ca4622fb2f054a56ae230ca214767704c768e7
Santosh <97425696+Sekharsantosh@users.noreply.github.com>
2022-08-24 13:16:13 +0530

    def is_prime(n):    if n==1:        return False    for i in range(2,int(n**0.5)+1):        if n%i==0:            return False    return True n=int(input()) k=0 f=0 p=0 a=[] for i in range(1,n//2):     for j in range(2,n//2+1):         if is_prime(i):             if is_prime(j):                 if i*j==n:                     k=1                     f=i                     p=j                     a.append(i)                     a.append(j)                     break a=set(sorted(a)) if k==0:     print(-1) else:     print(*a)

--- Find_two_distinct_prime_numbers_with_given_product.py
+++ Find_two_distinct_prime_numbers_with_given_product.py
@@ -1,23 +1,28 @@
 def is_prime(n):
-    if n==1:
-        return False
-    elif n>1:
-        for i in range(2,int(n**0.5)+1):
-            if n%i==0:
-                return False
-        return True
+   if n==1:
+       return False
+   for i in range(2,int(n**0.5)+1):
+       if n%i==0:
+           return False
+   return True
 n=int(input())
 k=0
+f=0
+p=0
 a=[]
-for i in range(1,n):
-    if is_prime(i):
-        a.append(i)
-for i in a:
-    for j in a:
-        if i<j:
-          if i!=j and (n%i==0 and n%j==0):
-             k=1
-             print(i,j)
-             break
-if k<=0:
+for i in range(1,n//2):
+    for j in range(2,n//2+1):
+        if is_prime(i):
+            if is_prime(j):
+                if i*j==n:
+                    k=1
+                    f=i
+                    p=j
+                    a.append(i)
+                    a.append(j)
+                    break
+a=set(sorted(a))
+if k==0:
     print(-1)
+else:
+    print(*a)

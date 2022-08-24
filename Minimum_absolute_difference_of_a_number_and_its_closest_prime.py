a3b2a5dd4383e02ea725ccdcf68a903db29f039f
Santosh <97425696+Sekharsantosh@users.noreply.github.com>
2022-08-24 12:30:45 +0530

    def is_prime(n):     if n==1:         return False     for i in range(2,int(n**0.5)+1):         if n%i==0:             return False     return True def display(a):     t=a     k=0     d=0     f=0     h=0     while True:         t=t+1         if is_prime(t):             k=t             break     while True:         t=t-1         if is_prime(t):             d=t             break     f=abs(k-a)     h=abs(a-d)     l=min(f,h)     if f<h:#47 51 53         print(f)     elif f>h:         print(h)     else:         print(l) n=int(input()) display(n)

--- Minimum_absolute_difference_of_a_number_and_its_closest_prime.py
+++ Minimum_absolute_difference_of_a_number_and_its_closest_prime.py
@@ -5,15 +5,33 @@ def is_prime(n):
         if n%i==0:
             return False
     return True
+def display(a):
+    t=a
+    k=0
+    d=0
+    f=0
+    h=0
+    while True:
+        t=t+1
+        if is_prime(t):
+            k=t
+            break
+    while True:
+        t=t-1
+        if is_prime(t):
+            d=t
+            break
+    f=abs(k-a)
+    h=abs(a-d)
+    l=min(f,h)
+    if f<h:#47 51 53 
+        print(f)
+    elif f>h:
+        print(h)
+    else:
+        print(l)
 n=int(input())
-t=n
-k=n
-while True:
-    if is_prime(t):
-        break
-    t=t+1
-while True:
-    if is_prime(k):
-        break
-    k=k-1
-print(min(t-n ,n-k))
+display(n)
+
+
+        

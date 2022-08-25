443076e6c68b3cb28e8902e75a3c626e80c2d47d
Santosh <97425696+Sekharsantosh@users.noreply.github.com>
2022-08-25 09:54:10 +0530

    def self_div(n):    t=n    k=0    while t:         k=0         r=t%10         if r>0 and n%r==0:             k=1         else:             k=0             break         t=t//10    if k==1:        return True    else:        return False n=int(input()) m=int(input()) for i in range(n,m+1):        if self_div(i):            print(i,end=" ")

--- Self_Dividing_Numbers.py
+++ Self_Dividing_Numbers.py
@@ -1,19 +1,22 @@
-def div(n):
-    s=n
-    c=0
-    while(s):
-        r=s%10
-        if r!=0:
-            if n%r==0 :
-                c=c+1
-        s=s//10
-    n=str(n)
-    if len(n)==c:
-        return True
-    else:
-        return False
+def self_div(n):
+   t=n
+   k=0
+   while t:
+        k=0
+        r=t%10
+        if r>0 and n%r==0:
+            k=1
+        else:
+            k=0
+            break
+        t=t//10
+   if k==1:
+       return True
+   else:
+       return False
 n=int(input())
 m=int(input())
 for i in range(n,m+1):
-    if div(i):
-        print(i,end=" ")
+       if self_div(i):
+           print(i,end=" ")
+    

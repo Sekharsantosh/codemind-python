bb4db43351e24028e2bef6bb8534a36f33428430
Santosh <97425696+Sekharsantosh@users.noreply.github.com>
2022-09-02 09:21:20 +0530

    n=input() vowels="aeiou" k=0 for i in vowels:      if i in n:            k=1      else:          k=0          break if k==1:     print("True") else:     print("False")

--- All_vowels_in_a_string.py
+++ All_vowels_in_a_string.py
@@ -1,11 +1,13 @@
 n=input()
-s=n.split()
-c=0
-v=set("aeiou")
-for i in n:
-    if i in v:
-        v.remove(i)
-if len(v)==0:       
-        print(True)
+vowels="aeiou"
+k=0
+for i in vowels:
+     if i in n:
+           k=1
+     else:
+         k=0
+         break
+if k==1:
+    print("True")
 else:
-        print(False)
+    print("False")

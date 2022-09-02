205e2e2923543f5d494fd83adb6564b5aa72fbec
Santosh <97425696+Sekharsantosh@users.noreply.github.com>
2022-09-02 09:00:34 +0530

    n=input() #print(n) a=[] for i in n.split():    # print(len(i))     a.append(len(i)) print(max(a))

--- maximum_word_lengh.py
+++ maximum_word_lengh.py
@@ -1,8 +1,7 @@
 n=input()
-k=n.split()
-for i in k:
-    m=len(str(i))
-for i in k:
-    if len(str(i))>m:
-        m=len(str(i))
-print(m)
+#print(n)
+a=[]
+for i in n.split():
+   # print(len(i))
+    a.append(len(i))
+print(max(a))

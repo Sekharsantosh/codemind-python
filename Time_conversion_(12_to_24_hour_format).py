316fa6e2ba5214d610bd935756fbed147c44fb2f
Santosh <97425696+Sekharsantosh@users.noreply.github.com>
2022-09-04 18:25:24 +0530

    def timeConversion(s):     if s[-2:] == "AM" and s[:2] == "12":         return "00" + s[2:-2]     elif s[-2:] == "AM":         return s[:-2]     elif s[-2:] == "PM" and s[:2] == "12":         return s[:-2]     else:        ans = int(s[:2]) + 12        return str(str(ans)+s[2:6])
    n=input()
    s=timeConversion(n)
    s=s.split()
    s="".join(s)
    print(s)

---
+++ Time_conversion_(12_to_24_hour_format).py
@@ -0,0 +1,16 @@
+def timeConversion(s):
+    if s[-2:] == "AM" and s[:2] == "12": 
+        return "00" + s[2:-2] 
+    elif s[-2:] == "AM": 
+        return s[:-2]
+    elif s[-2:] == "PM" and s[:2] == "12": 
+        return s[:-2] 
+    else:
+       ans = int(s[:2]) + 12
+       return str(str(ans)+s[2:6]) 
+
+n=input()
+s=timeConversion(n)
+s=s.split()
+s="".join(s)
+print(s)

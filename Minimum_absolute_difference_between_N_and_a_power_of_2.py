4b46dc58cd2daf4ea1dc99219d1a8b671c611beb
Santosh <97425696+Sekharsantosh@users.noreply.github.com>
2022-09-04 09:44:40 +0530

    n=int(input()) #print(n) k=0 z=0 for i in range(1,n//2+1):     if(2**i>n):         k=2**i         break for i in range(1,n//2+1):     if (2**i<=n):           z=2**i #print(z,k) #print(k-n) if(z==n or k==n):     print("0") elif(k-n<n-z):     print(k-n) else:     print(n-z)

---
+++ Minimum_absolute_difference_between_N_and_a_power_of_2.py
@@ -0,0 +1,19 @@
+n=int(input())
+#print(n)
+k=0
+z=0
+for i in range(1,n//2+1):
+    if(2**i>n):
+        k=2**i
+        break
+for i in range(1,n//2+1):
+    if (2**i<=n):
+          z=2**i
+#print(z,k)
+#print(k-n)
+if(z==n or k==n):
+    print("0")
+elif(k-n<n-z):
+    print(k-n)
+else:
+    print(n-z)
